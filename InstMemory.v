7ed1373e22f6b015d485d2eb444a0c59462fa302
SeyyedAhmad RokniHosseini <seyyedahmadrokni@ut.ac.ir>
2023-06-01 13:18:36 +0330

    memory wrote with readmemb

--- InstMemory.v
+++ InstMemory.v
@@ -1,10 +1,14 @@
-module InstMemory(a, inst);
-    input [31:0] a;
+module InstMemory(rst, a, inst);
+    input [15:0] a;
     output reg [31:0] inst;
     wire [31:0] address;
     assign address = {16'b0,a};
     reg [31:0] im [16000:0];
+    always @(posedge rst)begin
+        $readmemb("C:/Users/SeyyedAhmadRokniHoss/Documents/CA_CA2/ram.txt", im);
+    end
+
     always @(a)begin
-        inst = im[a];
+        inst = im[address];
     end
 endmodule
---
+++ ram_.v
@@ -0,0 +1,17 @@
+module ram_(rst, add, out);
+    input rst;
+    input [2:0] add;
+    reg [7:0] mem [7:0];
+    output reg [7:0] out;
+    always@(posedge rst)begin
+        $readmemb("C:/Users/SeyyedAhmadRokniHoss/Documents/CA_CA2/ram.txt", mem);
+    end
+
+    always@(add) begin
+        out = mem[add];
+    end
+
+
+
+
+endmodule
