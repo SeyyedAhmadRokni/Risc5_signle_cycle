4073b65288514dcd1815c698589e9321b7bc9a8d
AliGhAliGh <73810908+AliGhAliGh@users.noreply.github.com>
2023-06-03 10:47:30 +0330

    forth

--- DataPath.v
+++ DataPath.v
@@ -11,11 +11,11 @@ module DataPath(clk, rst, imm_op, alu_op, reg_we, mem_we, m4_1_cnt, m4_2_cnt, m2
 
     Pc pc(clk, rst, m4_1_out, pc_out);
     InstMemory im(rst, pc_out, inst);
-    RegMem regmem(clk, reg_we, inst[19:15], inst[24:20], inst[11:7], reg_wd, reg_rd1, reg_rd2);
+    RegMem regmem(clk, rst, reg_we, inst[19:15], inst[24:20], inst[11:7], reg_wd, reg_rd1, reg_rd2);
     ImmExtend imx(imm_op, inst, imm_ext_out);
     Alu alu(alu_op, reg_rd1, m2_1_out, alu_out, Zero);
     Mem memory(clk, rst, alu_out, mem_we, reg_rd2, mem_out);
-    Mux4 m4_1(add_1_out, add_2_out, m4_2_out, 32'b0, m4_1_cnt, m4_1_out);
+    Mux4 m4_1(add_2_out, add_1_out, m4_2_out, 32'b0, m4_1_cnt, m4_1_out);
     Mux4 m4_2(alu_out, mem_out, m2_3_out, 32'b0, m4_2_cnt, m4_2_out);
     Mux2 m2_1(reg_rd2, imm_ext_out, m2_1_cnt, m2_1_out);
     Mux2 m2_2(m4_2_out, add_2_out, m2_2_cnt, m2_2_out);
--- RegMem.v
+++ RegMem.v
@@ -1,12 +1,14 @@
-module RegMem(clk, we, a1, a2, a3, wd, rd1, rd2);
-    input clk, we;
+module RegMem(clk, rst, we, a1, a2, a3, wd, rd1, rd2);
+    input clk, we, rst;
     input [4:0] a1, a2, a3;
     input [31:0] wd;
     output[31:0] rd1, rd2;
-    reg [31:0] regmem [0:31];
+    reg [31:0] regmem [31:0];
     assign rd1 = regmem[a1];
     assign rd2 = regmem[a2];
-    always @(posedge clk)begin
+    always @(posedge clk, posedge rst)begin
+        if (rst)
+            regmem[0] = 32'b0;
         if (clk & we)
             regmem[a3] = wd;      
     end
